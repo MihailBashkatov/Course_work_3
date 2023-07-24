1d4e8d0a7ab1d520be09313bba913e0a51d1e7fd
Mihail <mihail.bashkatov@gmail.com>
2023-07-24 14:38:56 +0200

    Class user created

---
+++ data/class_user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, id=25, state=None, date=None, operationAmount=None, description=None, from_=None, to=None):
+
+        self.id = id
+        self.state = state
+        self.date = date
+        self.opertionAmoint = operationAmount
+        self.description = description
+        self.from_ = from_
+        self.to = to
+
