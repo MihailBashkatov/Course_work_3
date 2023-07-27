b0233770198fce762be4128f1ea8ca8c7b58ff3f
Mihail <mihail.bashkatov@gmail.com>
2023-07-27 11:16:38 +0200

    Tests for utils.py are added

---
+++ tests/test_utils.py
@@ -0,0 +1,46 @@
+from data.filedata import FILEDATA
+from src.utils import *
+
+filename = FILEDATA
+
+def test_load_file():
+    assert type(load_file(filename)) == list
+
+
+def test_dict_consistency():
+    data_base = load_file(filename)
+    assert type(dict_consistency(data_base)) == list
+    for element in dict_consistency(data_base):
+        assert len(element) == 7
+
+
+formatted_dict = dict_consistency(load_file(filename))
+def test_objects_file():
+    assert type(objects_file(formatted_dict)) == list
+    assert len(objects_file(formatted_dict)) == len(formatted_dict)
+
+
+gen_object = objects_file(formatted_dict)
+def test_action_date():
+    assert type(action_date(gen_object)) == list
+    assert action_date(gen_object)[0].date.count('-') == 2
+    assert len(action_date(gen_object)[0].date) == 10
+
+
+base_date = action_date(gen_object)
+# def test_order_actions_date():
+#     assert type(order_actions_date(base_date)) == list
+    # for element in order_actions_date(base_date):
+    #     assert element[-5] == '-'
+
+
+order_date = order_actions_date(base_date)
+def test_order_actions_objets():
+    assert type(order_actions_objets(order_date, base_date)) == list
+
+
+
+order_object = order_actions_objets(order_date, base_date)
+def test_get_executed():
+    assert type(get_executed(order_object)) == list
+    assert len(get_executed(order_object)) == 5
