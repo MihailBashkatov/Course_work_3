565a9b613c3064f0fecac0d1edffb219e5966e7b
Mihail <mihail.bashkatov@gmail.com>
2023-07-27 20:53:05 +0200

    Added fixture func for test_class_user.py

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from data.filedata import FILEDATA_TESTS
+from src.utils import *
+
+
+@pytest.fixture
+def final_result():
+    base_data = load_file(FILEDATA_TESTS)
+    dict_formatted = dict_consistency(base_data)
+    gen_objects = objects_file(dict_formatted)
+    base_date = action_date(gen_objects)
+    order_date = order_actions_date(base_date)
+    order_object_by_date = order_actions_objets(order_date, base_date)
+    return get_executed(order_object_by_date)
