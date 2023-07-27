568e22d8b6def70b983bd2cea8e3f04976531588
Mihail <mihail.bashkatov@gmail.com>
2023-07-27 20:54:12 +0200

    Refactored test with fixture

--- tests/test_class_user.py
+++ tests/test_class_user.py
@@ -1,41 +1,24 @@
-from data.filedata import FILEDATA
-from src.utils import *
 
-
-base_data = load_file(FILEDATA)
-dict_formatted = dict_consistency(base_data)
-gen_objects = objects_file(dict_formatted)
-base_date = action_date(gen_objects)
-order_date = order_actions_date(base_date)
-order_object_by_date = order_actions_objets(order_date, base_date)
-final_result = get_executed(order_object_by_date)
-
-
-def test_get_operation_amount():
-
+def test_get_operation_amount(final_result):
     for user in final_result:
         assert '.' in user.get_operation_amount()
         assert user.get_operation_amount()[0].isdigit() == True
         assert type(user.get_operation_amount()) == str
 
 
-def test_get_currency():
-    final_result = get_executed(order_object_by_date)
+def test_get_currency(final_result):
     for user in final_result:
         assert type(user.get_currency()) == str
         assert user.get_currency()[0].isalpha() == True
 
 
-def test_get_from():
-    final_result = get_executed(order_object_by_date)
+def test_get_from(final_result):
     for user in final_result:
         assert type(user.get_from()) == str
 
 
-def test_get_to():
-    final_result = get_executed(order_object_by_date)
+def test_get_to(final_result):
     for user in final_result:
         assert type(user.get_to()) == str
         assert user.get_to()[-4:-1].isdigit() == True
         assert user.get_to()[-6:-4] == '*' * 2
-
