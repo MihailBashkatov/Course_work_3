05d8f25d4662a8d7c748ea3fde2d01a44924ecbc
Mihail <mihail.bashkatov@gmail.com>
2023-07-28 11:35:00 +0200

    Docstrings to test_class_user.py are added

--- tests/test_class_user.py
+++ tests/test_class_user.py
@@ -1,5 +1,8 @@
 
 def test_get_operation_amount(final_result):
+    """ Asserting if '.' is in the return, if first element is digit
+    and if final value is string"""
+
     for user in final_result:
         assert '.' in user.get_operation_amount()
         assert user.get_operation_amount()[0].isdigit() == True
@@ -7,17 +10,25 @@ def test_get_operation_amount(final_result):
 
 
 def test_get_currency(final_result):
+    """ Asserting if  first element is alpha
+        and if final value is string"""
+
     for user in final_result:
         assert type(user.get_currency()) == str
         assert user.get_currency()[0].isalpha() == True
 
 
 def test_get_from(final_result):
+    """ Asserting if final value is string"""
+
     for user in final_result:
         assert type(user.get_from()) == str
 
 
 def test_get_to(final_result):
+    """ Asserting if final value is string, if 4 last values are digits
+    and 5-6 values from the end are '*'"""
+
     for user in final_result:
         assert type(user.get_to()) == str
         assert user.get_to()[-4:-1].isdigit() == True
